55c0f548fc044b34382afd07ae197bfacd04e0b8
reubber <reubbers@alu.ufc.br>
2021-02-09 16:19:16 -0300

    add spacer and pizza component on the main Content

--- sistema-pedidos/src/pages/main/main.js
+++ sistema-pedidos/src/pages/main/main.js
@@ -2,15 +2,22 @@ import React, { useState, useContext } from 'react'
 import styled from 'styled-components'
 import {
   AppBar,
+  Grid,
   Toolbar as MaterialUiToolbar,
   IconButton,
   Typography,
   Menu,
-  MenuItem
+  MenuItem,
+  Paper,
+  withStyles
 } from '@material-ui/core'
 import { AccountCircle } from '@material-ui/icons'
 import { ReactComponent as MainLogo } from 'images/logopedida.svg'
 import { AuthContext } from 'contexts/auth'
+
+const Content = styled.main`
+  padding: 20px;
+`
 
 const Logo = styled(MainLogo)`
   width: initial;
@@ -31,10 +38,46 @@ const Toolbar = styled(MaterialUiToolbar)`
   max-width: 960px;
   margin: 0 auto;
 `
+const style = (theme) => {
+  return {
+    main: theme.mixins.toolbar
+  }
+}
+
+const Spacer = withStyles(style)(({ classes }) => (
+  <div className={classes.main} />
+))
+
+const PizzaSizes = [
+  {
+    id: 0,
+    name: 'Pequena',
+    size: 28,
+    slices: 2,
+    flavours: 1
+  },
+
+  {
+    id: 1,
+    name: 'Média',
+    size: 30,
+    slices: 6,
+    flavours: 2
+  },
+  {
+    id: 2,
+    name: 'Grande',
+    size: 32,
+    slices: 8,
+    flavours: 3
+  }
+
+]
 
 function Main () {
   const [anchorElement, setAnchorElement] = useState(null)
   const { userInfo, logout } = useContext(AuthContext)
+  const userName = userInfo.user.displayName.split(' ')[0]
 
   const handleOpenMenu = (e) => {
     setAnchorElement(e.target)
@@ -45,30 +88,55 @@ function Main () {
   }
 
   return (
-    <AppBar>
-      <Toolbar>
-        <LogoContainer>
-          <Logo />
-        </LogoContainer>
+    <>
+      <AppBar>
+        <Toolbar>
+          <LogoContainer>
+            <Logo />
+          </LogoContainer>
 
-        <Typography>Olá {userInfo.user.displayName.split(' ')[0]} :)</Typography>
+          <Typography>Olá {userName} :)</Typography>
 
-        <IconButton color='inherit' onClick={handleOpenMenu}>
-          <AccountCircle />
-        </IconButton>
+          <IconButton color='inherit' onClick={handleOpenMenu}>
+            <AccountCircle />
+          </IconButton>
 
-        <Menu
-          open={Boolean(anchorElement)}
-          onClose={handleClose}
-          anchorEl={anchorElement}
-        >
-          <MenuItem onClick={logout}>Sair</MenuItem>
-        </Menu>
+          <Menu
+            open={Boolean(anchorElement)}
+            onClose={handleClose}
+            anchorEl={anchorElement}
+          >
+            <MenuItem onClick={logout}>Sair</MenuItem>
+          </Menu>
 
-      </Toolbar>
+        </Toolbar>
+      </AppBar>
 
-    </AppBar>
+      <Spacer />
 
+      <Content>
+        <Grid container direction='column' alignItems='center'>
+          <Typography variant='h3'>
+            o que vai ser hoje, {userName}?
+          </Typography>
+          <Typography variant='h4'>
+            Escolha o tamanho da pizza
+          </Typography>
+        </Grid>
+
+        <Grid container spacing={10}>
+          {PizzaSizes.map((pizza) => (
+            <Grid item key={pizza.id} xs={4}>
+              <Paper style={{ padding: 20 }}>
+                <div>{pizza.size}cm</div>
+                <Typography>{pizza.name}</Typography>
+                <Typography>{pizza.slices} fatias, {pizza.flavours} sabores</Typography>
+              </Paper>
+            </Grid>
+          ))}
+        </Grid>
+      </Content>
+    </>
   )
 }
 
